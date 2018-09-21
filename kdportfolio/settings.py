8546a054ec01ee76b6a09ee17205a49f406b2747
K. Dupont <dupontke@yahoo.com>
2018-09-21 14:42:03 -0400

    SK Change, updated pi images and size

--- kdportfolio/settings.py
+++ kdportfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from keys import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'khjw^(9mk^)qx*!!6p3%=w(!kkix4+u!rg_o5u^c6svk*wbmnh'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
